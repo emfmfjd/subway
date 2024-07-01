5d3419b25b771d2ae2f101104f62eb7e8a989b7b
rulloo <yesong1250@naver.com>
2024-07-01 18:02:06 +0900

    1~8호선 html 이미지 업데이트 완료

--- mini3/subway/urls.py
+++ mini3/subway/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = 'subway'
 
 urlpatterns = [
-    path('3호선/', views.line3, name='3호선')
+    path('1호선/', views.line1, name='1호선'),
+    path('2호선/', views.line2, name='2호선'),
+    path('3호선/', views.line3, name='3호선'),
+    path('4호선/', views.line4, name='4호선'),
+    path('5호선/', views.line5, name='5호선'),
+    path('6호선/', views.line6, name='6호선'),
+    path('7호선/', views.line7, name='7호선'),
+    path('8호선/', views.line8, name='8호선'),
 ]
--- mini3/subway/views.py
+++ mini3/subway/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def line1(request):
+    return render(request, 'subway/line1.html')
+
+def line2(request):
+    return render(request, 'subway/line2.html')
+
 def line3(request):
-    return render(request,'subway/line3.html')
+    return render(request, 'subway/line3.html')
+
+def line4(request):
+    return render(request, 'subway/line4.html')
+
+def line5(request):
+    return render(request, 'subway/line5.html')
+
+def line6(request):
+    return render(request, 'subway/line6.html')
+
+def line7(request):
+    return render(request, 'subway/line7.html')
+
+def line8(request):
+    return render(request, 'subway/line8.html')
