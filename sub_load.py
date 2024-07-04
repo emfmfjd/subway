97ac439ad3e9a3c526e8806f3139d42ce6cdc3eb
2chung1 <cw3714@naver.com>
2024-07-04 12:20:21 +0900

    3

--- sub_load.py
+++ sub_load.py
@@ -30,44 +30,25 @@ dag = DAG(
     catchup=False,
 )
 
-#1  
-def get_info_realtime():
-    url = 'http://swopenapi.seoul.go.kr/api/subway/6a704b554e6b73653638687a4a6747/json/realtimeStationArrival/ALL/1/5'
-    r = requests.get(url).json()
-    data = pd.json_normalize(r,record_path=['realtimeArrivalList'])
-    filtered_data = {key: r['errorMessage'][key] for key in ['status', 'code', 'message']}
-    mes = pd.DataFrame([filtered_data])  
-    sample = data[['subwayId','statnNm','arvlMsg2','arvlMsg3','arvlCd','barvlDt','bstatnNm','updnLine','lstcarAt']]
-    sample['subwayId'] = sample['subwayId'].astype(int)
-    sample = sample[(sample['subwayId'] > 1000) & (sample['subwayId'] < 1010)].sort_values(by='subwayId', ascending=True)
-    for col in mes.columns:
-        sample[col] = mes[col].iloc[0]
-    return sample
-    
 
 #load_realtime_position
 def get_info_position():
     arrive = pd.DataFrame()
-    for i in range(1, 10):
-        url = f'http://swopenapi.seoul.go.kr/api/subway/7874454f7a637733313033434a626b61/json/realtimePosition/0/1000/{i}호선'
-        r = requests.get(url).json()
-        data = pd.json_normalize(r, record_path=['realtimePositionList'])
-        filtered_data = {key: r['errorMessage'][key] for key in ['status', 'code', 'message']}
-        mes = pd.DataFrame([filtered_data])
-        sample = data[['subwayId', 'statnId', 'statnNm','updnLine']]
-        sample['subwayId'] = sample['subwayId'].astype(int)
-        sample = sample[(sample['subwayId'] > 1000) & (sample['subwayId'] < 1009)].sort_values(by='subwayId', ascending=True)
-        for col in mes.columns:
-            sample[col] = mes[col].iloc[0]
-        arrive = pd.concat([arrive, sample], ignore_index=True)
+    url = 'http://swopenapi.seoul.go.kr/api/subway/7874454f7a637733313033434a626b61/json/realtimePosition/0/1000/3호선'
+    r = requests.get(url).json()
+    data = pd.json_normalize(r, record_path=['realtimePositionList'])
+    filtered_data = {key: r['errorMessage'][key] for key in ['status', 'code', 'message']}
+    mes = pd.DataFrame([filtered_data])
+    sample = data[['subwayNm', 'statnNm', 'statnTnm', 'trainSttus', 'updnLine']]
+    sample['subwayId'] = sample['subwayId'].astype(int)
+    for col in mes.columns:
+        sample[col] = mes[col].iloc[0]
+    arrive = pd.concat([arrive, sample], ignore_index=True)
     return arrive
 
 #2
-def merge_send2kafka():
-    realtime = get_info_realtime()
+def processing_data():
     position = get_info_position()
-    merged_df = pd.merge(realtime, position, on=['subwayId', 'statnNm'], suffixes=('', '_realtime'), how='inner')
-    merged_df.drop_duplicates(subset = 'arvlMsg2', keep = 'first', inplace = True)
     sub_Nm = {
     '1001' : '1호선',
     '1002' : '2호선',
@@ -77,14 +58,12 @@ def merge_send2kafka():
     '1006' : '6호선',
     '1007' : '7호선',
     '1008' : '8호선',}
-    arr_cd = {
+    train_stat = {
     '0' : '진입',
     '1' : '도착',
     '2' : '출발',
     '3' : '전역출발',
-    '4' : '전역진입',
-    '5' : '전역도착',
-    '99' : '운행중'}
+    }
     
     updnLine_Nm = {
         '0' : '상행/내선',
@@ -92,13 +71,13 @@ def merge_send2kafka():
     }
     
     for i,k in sub_Nm.items():
-        merged_df['subwayId'] = merged_df['subwayId'].astype(str).apply(lambda x : x.replace(i,k))
-    for l,m in arr_cd.items():
-        merged_df['arvlCd'] = merged_df['arvlCd'].astype(str).apply(lambda x : x.replace(l,m))
+        position['subwayId'] = position['subwayId'].astype(str).apply(lambda x : x.replace(i,k))
     for j,n in updnLine_Nm.items():
-        merged_df['updnLine'] = merged_df['updnLine'].astype(str).apply(lambda x : x.replace(j,n))
+        position['updnLine'] = position['updnLine'].astype(str).apply(lambda x : x.replace(j,n))
+    for z,c in train_stat():
+        position['trainSttus'] = position['trainSttus'].astype(str).apply(lambda x : x.replace(z,c))
         
-    return merged_df.to_csv('/home/hadoop/workspace/realtime_subway.csv', index = False, encoding = 'utf-8-sig')
+    return position.to_csv('/home/hadoop/workspace/realtime_subway.csv', index = False, encoding = 'utf-8-sig')
 
 def save2Hadoop():
     classpath = subprocess.Popen(["/home/hadoop/hadoop/bin/hdfs", "classpath", "--glob"], stdout=subprocess.PIPE).communicate()[0]
@@ -108,23 +87,21 @@ def save2Hadoop():
     hdfs_path = '/subway_project/subway_data.csv'
     with hdfs.open_output_stream(hdfs_path) as f:
         df.to_csv(f, index=False)
+        
+def send2kafka():
+    pass
 
- 
-first_load = PythonOperator(task_id='first_load',
-                python_callable= get_info_realtime,
-                dag=dag
-              )
 
-second_load = PythonOperator(task_id = 'second_data',
+position_load = PythonOperator(task_id = 'position_data',
                            python_callable = get_info_position,
                            dag = dag)
 
-merge_data = PythonOperator(task_id = 'merge_data',
-                             python_callable = merge_send2kafka,
+processing = PythonOperator(task_id = 'processing_data',
+                             python_callable = processing_data,
                              dag = dag)
 
 send_data = PythonOperator(task_id = 'send_data',
                              python_callable = save2Hadoop,
                              dag = dag)
 
-first_load >> second_load >> merge_data >> send_data
+position_load >> processing
