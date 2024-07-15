9233b80d0714ecd874e908a76fcdfb3fa7dfef36
2chung1 <cw3714@naver.com>
2024-07-15 09:07:00 +0900

    kafka to spring

---
+++ mykafka/src/main/java/com/kafka/mykafka/KafkaConfig.java
@@ -0,0 +1,50 @@
+package com.kafka.mykafka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String servers;
+
+    @Bean
+public ConsumerFactory<String, KafkaMsgVO> consumerFactory() {
+    Map<String, Object> props = new HashMap<>();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "subway_con");
+    props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.kafka.mykafka");
+    props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "com.kafka.mykafka.KafkaMsgVO");
+    props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 50);
+
+    JsonDeserializer<KafkaMsgVO> deserializer = new JsonDeserializer<>(KafkaMsgVO.class);
+    deserializer.configure(props, false);
+
+    return new DefaultKafkaConsumerFactory<>(
+            props,
+            new ErrorHandlingDeserializer<>(new StringDeserializer()),
+            new ErrorHandlingDeserializer<>(deserializer)
+    );
+}
+
+@Bean
+public ConcurrentKafkaListenerContainerFactory<String, KafkaMsgVO> kafkaListenerContainerFactory() {
+    ConcurrentKafkaListenerContainerFactory<String, KafkaMsgVO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    factory.setConsumerFactory(consumerFactory());
+    return factory;
+}
+}
---
+++ mykafka/src/main/java/com/kafka/mykafka/KafkaController.java.backup
@@ -0,0 +1,26 @@
+package com.kafka.mykafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public String sendMessageJson(@RequestBody KafkaMsgVO message) {
+        this.producer.sendMessage(message);
+        return "success";
+    }
+}
